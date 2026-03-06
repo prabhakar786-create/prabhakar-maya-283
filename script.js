9a4d5a1728a504ce1a83f344c25af278c38a7a14
prabhakar786-create <prabhakar786@gmail.com>
2026-03-06 15:56:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+﻿document.addEventListener("DOMContentLoaded", function(){
+
+document.getElementById("volunteerForm")
+.addEventListener("submit", function(e){
+
+e.preventDefault();
+
+alert("Volunteer registration submitted");
+
+});
+
+});
