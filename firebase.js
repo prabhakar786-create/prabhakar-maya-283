dd621df0731a5274889e05da9e851bee9dd23f52
prabhakar786-create <prabhakar786@gmail.com>
2026-03-10 21:57:14 +0530

    Update and rename volunteer.html to firebase.js

---
+++ firebase.js
@@ -0,0 +1,16 @@
+
+var firebaseConfig={
+
+apiKey:"YOUR_API_KEY",
+authDomain:"YOUR_PROJECT.firebaseapp.com",
+databaseURL:"YOUR_DATABASE_URL",
+projectId:"YOUR_PROJECT",
+storageBucket:"YOUR_PROJECT.appspot.com",
+messagingSenderId:"XXXX",
+appId:"XXXX"
+
+};
+
+firebase.initializeApp(firebaseConfig);
+
+var database=firebase.database();
